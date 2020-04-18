b7b74fd8ed76884835b9ab44fe0d39e890414179
Mac <mac@Macde-MacBook-Pro.local>
2020-04-18 14:57:40 +0800

    r3.py

---
+++ r3.py
@@ -0,0 +1,17 @@
+#r3
+
+
+lines = []
+with open('3.txt','r', encoding = 'utf - 8 - sig') as f:
+	for line in f :
+		lines.append(line.strip())
+
+
+for line in lines :
+	s = line.split(' ')
+
+	time = s[0][:5]
+	name = s[0][5:]
+	word = s[1:]
+	print(name,word)
+	
