40e6f5e94a16abbfeeb8cb2ea65169196cf6767c
Mac <mac@Macde-MacBook-Pro.local>
2020-04-18 11:28:41 +0800

    chat function

---
+++ c1.py
@@ -0,0 +1,40 @@
+#chat
+
+
+def read_file(filename):
+	lines = []
+	with open (filename,'r',encoding = 'utf - 8 - sig') as f:
+		for line in f:
+			lines.append(line.strip())
+
+	return lines
+
+def convert_file(lines):
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+
+		if person:
+			new.append(person + ': ' + line )
+	return new
+
+def write_file(filename,lines):
+	with open(filename,'w', encoding = 'utf - 8 - sig') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+
+	filename = 'input.txt'
+	lines = read_file(filename)
+	lines = convert_file(lines)
+	write_file('output.txt',lines)
+
+main()
+
